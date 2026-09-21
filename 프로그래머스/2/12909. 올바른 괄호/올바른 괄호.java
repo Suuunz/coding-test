29ff64b4cc1cc611380329a90023526545fdfe57
Suuunz <ysj18805@khu.ac.kr>
2026-09-21 15:21:54 +0900

    [level 2] Title: 올바른 괄호, Time: 2.73 ms, Memory: 57.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        int open = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                open++;
+            } else {
+                open--;
+                if (open < 0) return false;
+            }
+        }
+
+        return open == 0;
+    }
+}
