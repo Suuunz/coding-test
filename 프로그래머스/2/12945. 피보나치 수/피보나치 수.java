59dd60bfd9c6455f839354d7a7acb60fd76da751
Suuunz <ysj18805@khu.ac.kr>
2026-09-24 06:34:30 +0900

    [level 2] Title: 피보나치 수, Time: 1.31 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int a = 0; // F(0)
+        int b = 1; // F(1)
+
+        for (int i = 2; i <= n; i++) {
+            int next = (a + b) % 1234567;
+
+            a = b;
+            b = next;
+        }
+
+        return b;
+    }
+}
